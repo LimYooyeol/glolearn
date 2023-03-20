3905b22f21a2870165ee696b6b6d9a083105811c
LimYooyeol <duf7040@naver.com>
2023-03-20 20:28:00 +0900

    목록 기능 추가

--- src/main/java/com/glolearn/newbook/dto/lecture/LectureDetailsDto.java
+++ src/main/java/com/glolearn/newbook/dto/lecture/LectureDetailsDto.java
@@ -7,6 +7,8 @@ import lombok.Data;
 public class LectureDetailsDto {
     private Long lectureId;
 
+    private Long courseId;
+
     private String courseTitle;
 
     private String lectureTitle;
@@ -16,6 +18,7 @@ public class LectureDetailsDto {
 
     public LectureDetailsDto(Lecture lecture) {
         this.lectureId = lecture.getId();
+        this.courseId = lecture.getCourse().getId();
         this.courseTitle = lecture.getCourse().getTitle();
         this.lectureTitle = lecture.getTitle();
         this.contents = lecture.getContents().getContents();
